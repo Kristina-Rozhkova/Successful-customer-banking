6df4043a3d8286a4ffe3d1eee747888b137f081d
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-10-29 23:12:42 +0400

    add masks.py and widget.py after scanning with lint group

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 
-def get_mask_card_number(card_number: Union[int]) -> str:
+def get_mask_card_number(card_number: Union[int | str]) -> str:
     """Функция маркировки номера банковской карты"""
     str_card_number = str(card_number)
     transformed_card_number = (
@@ -10,14 +10,14 @@ def get_mask_card_number(card_number: Union[int]) -> str:
     return transformed_card_number
 
 
-def get_mask_account(account_number: Union[int]) -> str:
+def get_mask_account(account_number: Union[int | str]) -> str:
     """Функция маркировки номера банковского счета"""
     str_account_number = str(account_number)
     transformed_account_number = "**" + str_account_number[-4:]
     return transformed_account_number
 
 
-#card_number = int(input("Введите номер карты: "))
-#account_number = int(input("Введите номер счета: "))
-#print(get_mask_card_number(card_number))
-#print(get_mask_account(account_number))
+# card_number = int(input("Введите номер карты: "))
+# account_number = int(input("Введите номер счета: "))
+# print(get_mask_card_number(card_number))
+# print(get_mask_account(account_number))
--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,27 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(numb: str) -> str:
-    """ Функция, которая обрабатывает информацию о картах и о счетах """
-    name_of_card = ''
-    if 'Счет' in numb:
-        return 'Счет ' + get_mask_account(numb)
+    """Функция, которая обрабатывает информацию о картах и о счетах"""
+    name_of_card = ""
+    if "Счет" in numb:
+        return "Счет " + get_mask_account(numb)
     else:
         number_card = numb[-16:]
         for letter in numb:
-            if letter.isalpha() or letter == ' ':
+            if letter.isalpha() or letter == " ":
                 name_of_card += letter
         return name_of_card + get_mask_card_number(number_card)
 
 
-def get_date(date_from_user:str) -> str:
+def get_date(date_from_user: str) -> str:
     """
     Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")
     """
-    correct_date = date_from_user[8:10] + '.' + date_from_user[5:7] + '.' + date_from_user[:4]
+    correct_date = date_from_user[8:10] + "." + date_from_user[5:7] + "." + date_from_user[:4]
     return correct_date
 
 
-#numb = input()
-#print(get_date(numb))
+# numb = input()
+# print(get_date(numb))
