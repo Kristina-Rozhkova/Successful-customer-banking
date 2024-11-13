e05064fb3188bb36998c00788d77344dc2c65b64
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-11-13 20:26:24 +0400

    init processing.py

---
+++ src/processing.py
@@ -0,0 +1,20 @@
+def filter_by_state(lists_of_dict_1: list[dict], state: str='EXECUTED') -> list[dict]:
+    """
+    Функция принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению
+    """
+
+    new_list = []
+    for dictionaries in lists_of_dict_1:
+        for value in dictionaries.values():
+            if value == state:
+                new_list.append(dictionaries)
+    return new_list
+
+
+def sort_by_date(lists_of_dict_2: list[dict], order: bool = True) -> list[dict]:
+    """
+    Функция возвращает список, отсортированный по дате
+    """
+    sorted_list = sorted(lists_of_dict_2, key=lambda date: date['date'], reverse=order)
+    return sorted_list
