8272a6c688eaa9fbdc408ea436b21309d0dc3fd9
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-11-13 20:34:58 +0400

    processing.py after testing with mypy, isort and flake8

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(lists_of_dict_1: list[dict], state: str='EXECUTED') -> list[dict]:
+def filter_by_state(lists_of_dict_1: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция принимает список словарей и опционально значение для ключа state, возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению
@@ -16,5 +16,5 @@ def sort_by_date(lists_of_dict_2: list[dict], order: bool = True) -> list[dict]:
     """
     Функция возвращает список, отсортированный по дате
     """
-    sorted_list = sorted(lists_of_dict_2, key=lambda date: date['date'], reverse=order)
+    sorted_list = sorted(lists_of_dict_2, key=lambda date: date["date"], reverse=order)
     return sorted_list
