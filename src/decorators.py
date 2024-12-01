5e62ba5e2f4ea0c48529ebf9718a09b652a1cb0f
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-12-01 23:49:21 +0400

    homework 11.2 done

---
+++ src/decorators.py
@@ -0,0 +1,47 @@
+import time
+from functools import wraps
+
+
+def log(filename=None):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            try:
+                start = time.time()
+                result = func(*args, **kwargs)
+                end = time.time()
+                time_for_work = end - start
+                message_for_log = (
+                    f"The result of working {func.__name__}: {result}\n" f"Time for working: {time_for_work:.20f}"
+                )
+                if filename:
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(message_for_log)
+                else:
+                    print(message_for_log)
+
+                return result
+            except Exception as error:
+                error_message = (
+                    f'{func.__name__} fell down with the error "{error}".\n'
+                    f"The arguments of function was {args}, {kwargs}"
+                )
+                if filename:
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(error_message)
+                else:
+                    print(error_message)
+                # raise error
+
+        return inner
+
+    return wrapper
+
+
+@log(filename="mylog.txt")
+def my_function(x: int | float, y: int | float) -> int | float:
+    "Функция суммы аргументов х и y"
+    return x + y
+
+
+my_function("190080", 5804654982)
