b7108a847af67bce035ae78711984a888951ea6d
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-12-12 23:31:46 +0400

    adding error handling

--- src/utils.py
+++ src/utils.py
@@ -7,15 +7,19 @@ json_path = r"..\data\operations.json"
 def get_financial_transactions(json_path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
     # если путь к файлу существует
-    if os.path.exists(json_path):
-        # если файл содержит данные
-        if json_path:
-            with open(json_path, 'r', encoding='utf-8') as file:
-                json_file = json.load(file)
-                # если файл содержит список данных
-                if isinstance(json_file, list):
-                    return json_file
-    return []
+    try:
+        if os.path.exists(json_path):
+            # если файл содержит данные
+            if json_path:
+                with open(json_path, 'r', encoding='utf-8') as file:
+                    json_file = json.load(file)
+                    # если файл содержит список данных
+                    if isinstance(json_file, list):
+                        return json_file
+    except FileNotFoundError:
+        return []
+    except Exception:
+        return []
 
 
 print(get_financial_transactions(json_path))
