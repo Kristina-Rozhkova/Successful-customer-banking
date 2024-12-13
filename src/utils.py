7fe93c68674280a4ae478d7b06d93df8db1de19f
Kristina Rozhkova <kristina.rozhkova.2018@list.ru>
2024-12-13 15:23:54 +0400

    upgrading utils.py and changing test_utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,25 @@
 import json
-import os
-
-json_path = r"..\data\operations.json"
 
 
-def get_financial_transactions(json_path: str) -> list:
+# json_path = r"..\data\operations.json"
+
+
+def get_financial_transactions(json_path: str) -> dict | list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
-    # если путь к файлу существует
     try:
-        if os.path.exists(json_path):
-            # если файл содержит данные
-            if json_path:
-                with open(json_path, 'r', encoding='utf-8') as file:
-                    json_file = json.load(file)
-                    # если файл содержит список данных
-                    if isinstance(json_file, list):
-                        return json_file
+        with open(json_path, 'r', encoding='utf-8') as file:
+            json_file = json.load(file)
+            # если файл содержит словарь данных
+            if isinstance(json_file, dict):
+                return json_file
+            else:
+                raise ValueError
     except FileNotFoundError:
+        return []
+    except ValueError:
         return []
     except Exception:
         return []
 
 
-print(get_financial_transactions(json_path))
+print(get_financial_transactions(r"..\data\operations2.json"))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,12 +6,11 @@ from src.utils import get_financial_transactions
 
 def test_get_financial_transactions_success():
     # успешное открытие файла и работу функции
-    with patch('os.path.exists', return_value=True):
-        with patch('builtins.open',
-                   mock_open(read_data=json.dumps([{'amount': 500, 'currency': 'USD'}]))) as mock_open_file:
-            result = get_financial_transactions('json_path')
-            assert result == json.loads(json.dumps([{'amount': 500, 'currency': 'USD'}]))
-            mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
+    with patch('builtins.open',
+               mock_open(read_data=json.dumps({'amount': 500, 'currency': 'USD'}))) as mock_open_file:
+        result = get_financial_transactions('json_path')
+        assert result == json.loads(json.dumps({'amount': 500, 'currency': 'USD'}))
+        mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
 
 
 def test_get_financial_transactions_not_found():
@@ -23,18 +22,16 @@ def test_get_financial_transactions_not_found():
 
 def test_get_financial_transactions_clear():
     # результат функции, если файл пустой
-    with patch('os.path.exists', return_value=True):
-        with patch('builtins.open', mock_open(read_data=json.dumps([]))) as mock_open_file:
-            result = get_financial_transactions('json_path')
-            assert result == json.loads(json.dumps([]))
-            mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
+    with patch('builtins.open', mock_open(read_data=json.dumps(''))) as mock_open_file:
+        result = get_financial_transactions('json_path')
+        assert result == json.loads(json.dumps([]))
+        mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
 
 
 def test_get_financial_transactions_not_list():
-    # результат функции, если файл содержит не список
-    with patch('os.path.exists', return_value=True):
-        with patch('builtins.open',
-                   mock_open(read_data=json.dumps({'amount': 500, 'currency': 'USD'}))) as mock_open_file:
-            result = get_financial_transactions('json_path')
-            assert result == json.loads(json.dumps([]))
-            mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
+    # результат функции, если файл содержит не словарь
+    with patch('builtins.open',
+               mock_open(read_data=json.dumps([{'amount': 500, 'currency': 'USD'}]))) as mock_open_file:
+        result = get_financial_transactions('json_path')
+        assert result == json.loads(json.dumps([]))
+        mock_open_file.assert_called_once_with('json_path', 'r', encoding='utf-8')
